3a79dc621aec8377eb0ba159b503901ad583dda2
Mayon-Francis <mayonfrancis@gmail.com>
2023-03-07 16:47:21 +0530

    Loader lol

---
+++ 3_loader/loader.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+#include <errno.h>    
+
+/* msleep(): Sleep for the requested number of milliseconds. */
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+
+    do {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
+
+int main()
+{
+    printf("-");
+    for (;;)
+    {
+        msleep(100);
+        printf("\b\\");
+        fflush(stdout); 
+        msleep(100);
+        printf("\b|");
+        fflush(stdout); 
+        msleep(100);
+        printf("\b/");
+        fflush(stdout); 
+        msleep(100);
+        printf("\b-");
+        fflush(stdout); 
+    }
+}
